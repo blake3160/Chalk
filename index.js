72cfaf21cda4e1ef19b28e1b5b6b9441e3754230
blake3160 <55066068+blake3160@users.noreply.github.com>
2019-11-07 18:26:55 -0600

    Update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.blue('Hello, this is a short story about social engineering based on what happened to me recently.'));
+log(chalk.red('Just so were on the same page social engineering is the act of pretending to be someone else to get information.'));
+log(chalk.green('So, I was playing Counter Strike yesterday minding my own, and out of no where someone messages me out of no where'));
+log(chalk.red.bold('They said that they accidentally reported my steam account and that I need to contact an Steam "admin".'));
+log(chalk.blue.bold('I went along with it for about half an hour until he gave me a link to someones steam profile who was this Steam "admin"'));
+log(chalk.green.bold('before you ask, I didnt open the link but insted googled the link to make sure it was not a phishing link.'));
+log(chalk.bgRed('to my surprise the link was a real steam profle page.'));
+log(chalk.bgBlue('however I new it was a scam because no major platform such as facebook, or in this case, steam does not have profiles or accounts that will message you for moderation reasons.'));
+log(chalk.bgGreen('I told this story because almost everything added up and for a soild half and hour I thought this scammer was telling the truth. and'));
+log(chalk.bgGreen('How they used a random users page to draw you in.'));
+log(chalk.green('Overall, social engineering scams are getting more, and more clever everday, and I warn everyone to watch out!'));
